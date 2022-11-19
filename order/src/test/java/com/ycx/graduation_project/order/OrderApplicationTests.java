9f8ff7794065279f06dc22cb86d22d4a01e9db23
xixixiaozi <1253324157@qq.com>
2022-11-19 20:59:50 +0800

    初始结构创建

---
+++ order/src/test/java/com/ycx/graduation_project/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycx.graduation_project.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ware/src/test/java/com/ycx/graduation_project/ware/WareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycx.graduation_project.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
