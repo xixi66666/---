bd61cd4f97bc20e540b78486befe2b20a786b1b7
xixixiaozi <1253324157@qq.com>
2023-02-24 15:53:08 +0800

    update gateway

--- gateway/src/main/java/com/ycx/graduation_project/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/ycx/graduation_project/gateway/GatewayApplication.java
@@ -2,8 +2,11 @@ package com.ycx.graduation_project.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//nacos注册发现
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class GatewayApplication {
 
     public static void main(String[] args) {
