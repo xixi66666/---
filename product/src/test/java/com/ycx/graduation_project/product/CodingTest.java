f96a2aab1d2613479649674edc35fce5fe2e006d
YangChenxi <1253324157@qq.com>
2023-04-09 13:54:41 +0800

    测试coding分支

---
+++ product/src/test/java/com/ycx/graduation_project/product/CodingTest.java
@@ -0,0 +1,15 @@
+package com.ycx.graduation_project.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author YangChenxi
+ */
+@SpringBootTest
+public class CodingTest {
+    @Test
+    public void test(){
+        System.out.println("杨晨曦真帅");
+    }
+}
