5d3b215252bb4f64f98432f0f3d8095fe27a938b
xixixiaozi <1253324157@qq.com>
2024-02-03 19:34:17 +0800

    切换腾讯云环境

--- product/src/main/java/com/ycx/graduation_project/product/controller/SearchController.java
+++ product/src/main/java/com/ycx/graduation_project/product/controller/SearchController.java
@@ -21,15 +21,15 @@ public class SearchController {
 //        System.out.println("进入product");
 //        return "redirect:https://search.jd.com/Search?keyword=%E4%BA%AC%E4%B8%9C%E5%95%86%E5%9F%8E%E6%89%8B%E6%9C%BA&enc=utf-8&wq=%E4%BA%AC%E4%B8%9C%E5%95%86%E5%9F%8E%E6%89%8B%E6%9C%BA&pvid=96df37cbd7064c609d1e867911a7c436";
 //    }
-//    @GetMapping("/list.html")
-//    public String list(){
-//        System.out.println("进入product");
-//        return searchFeignService.search();
-//}
-@GetMapping("/list.html")
-public String list(){
-    System.out.println("进入product");
-    return "redirect:http://localhost:8090/";
+    @GetMapping("/list.html")
+    public String list(){
+        System.out.println("进入product");
+        return searchFeignService.search();
 }
+//@GetMapping("/list.html")
+//public String list(){
+//    System.out.println("进入product");
+//    return "redirect:http://localhost:8090/";
+//}
 
 }
